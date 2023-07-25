c2548e590359da71a9086bf3676bf72d850bb8cb
Benjamin Walter <86640585+benjamin-walter@users.noreply.github.com>
2023-07-25 02:16:01 -0300

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE hackathon_db;
+
+use hackathon_db;
+
+Create table if not EXISTS users  (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT Primary Key,
+email Varchar(100) UNIQUE NOT NULL,
+nick Varchar(40) UNIQUE NOT NULL,
+senha Varchar(100) NOT NULL,
+login_times Int DEFAULT 0 NOT NULL,
+last_login_bonus int NOT NULL,
+cards VARCHAR(2048)
+);
